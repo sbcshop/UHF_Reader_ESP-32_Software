d5a3b924cdb0cbd4b6a63056a8179d155e55aeef
sbcshop3 <129948335+sbcshop3@users.noreply.github.com>
2023-08-16 18:47:42 +0530

    Update Uhf.h

--- examples/Demo_UHFmodule_test/Uhf.h
+++ examples/Demo_UHFmodule_test/Uhf.h
@@ -9,6 +9,8 @@ static byte HARDWARE_VERSION[]    = {0XBB,0X00,0X03,0X00,0X01,0X00,0X04,0X7E};
 static byte MULTIPLE_READ[]   = {0XBB, 0X00, 0X27, 0X00, 0X03, 0X22, 0X27, 0X10, 0X83, 0X7E};
 static byte SINGLE_READ[]     = {0XBB,0X00,0X22,0X00,0X00,0X22,0X7E};
 static byte STOP_READ[]       = {0XBB,0X00,0X28,0X00,0X00,0X28,0X7E};
+static byte SET_REGION_EU[]   = {0xBB,0x00,0x07,0x00,0x01,0x03,0x0B,0x7E}; // For setting EU Region
+static byte SET_REGION_US[]   = {0xBB,0x00,0x07,0x00,0x01,0x02,0x0A,0x7E}; // For Setting US Region
 
 static String asciiVal;
 
