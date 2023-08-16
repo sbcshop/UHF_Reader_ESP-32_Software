ee8a9407b2a97a2cac126c0090425056fd1b1e2c
sbcshop3 <129948335+sbcshop3@users.noreply.github.com>
2023-08-16 18:47:16 +0530

    Update Demo_UHFmodule_test.ino

--- examples/Demo_UHFmodule_test/Demo_UHFmodule_test.ino
+++ examples/Demo_UHFmodule_test/Demo_UHFmodule_test.ino
@@ -16,6 +16,26 @@
 #define COMMANDS SINGLE_READ
 //#define COMMANDS STOP_READ
 
+// Function to setup region, if module range is less. Calling this function sets EU or US region as per requirement
+// For EU region add command -> SET_REGION_EU
+// For US region add command -> SET_REGION_US
+void setRegion(){
+  mySerial.write(SET_REGION_EU, sizeof(SET_REGION_EU)); // send command to UHF module for region configuration
+  delay(100);
+  int len = mySerial.available();
+  while (len <= 0){
+    Serial.print("wait..\n");
+    delay(500);
+    mySerial.write(SET_REGION_EU, sizeof(SET_REGION_EU)); // send command to UHF module for region configuration
+    len = mySerial.available();
+  }
+  
+  if (len > 0) {
+    mySerial.readBytes(container, len); // Read Response from UHF module, and parse frame received
+    frameParse(container, len); // This process done in Uhf.cpp file
+  }
+}
+
 void setup() {
   pinMode(21,OUTPUT);
   digitalWrite(21,LOW); // LOW - to enable the module and HIGH to disable the module
@@ -23,6 +43,9 @@ void setup() {
   mySerial.begin(115200, SERIAL_8N1, 18, 17);
   Serial.begin(115200);
   mySerial.setTimeout(2000);
+
+  //Uncomment below function to set region to increase range of UHF module if default getting low range 
+  //setRegion(); 
 }
 
 void loop() {
